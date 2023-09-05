c052ee442a990ad85a46e8d90154ca77951c8f27
NTHOscar <72460275+NTHOscar@users.noreply.github.com>
2023-09-05 23:30:57 +0800

    add try/catch in create TODO

--- app/todo/page.js
+++ app/todo/page.js
@@ -4,15 +4,19 @@ import styles from './TODO.module.css';
 async function getTODOList() {
     // const db = new PocketBase('http://127.0.0.1:8090');
     // const result = await db.records.getList('notes');
-    const res = await fetch('http://127.0.0.1:8090/api/collections/todo/records?page=1&perPage=30', { cache: 'no-store' });
-    const data = await res.json();
-    // console.log(data);
-    return data?.items;
+    try{
+        const res = await fetch('http://127.0.0.1:8090/api/collections/todo/records?page=1&perPage=30', { cache: 'no-store' });
+        const data = await res.json();
+        // console.log(data);
+        return data?.items;
+    } catch(err){
+        console.log(err);
+    }
   }
 
 export default async function TODOList() {
     const todoList = await getTODOList();
-    console.log(todoList);
+    // console.log(todoList);
     return(
         <div>
             <h1>To-Do List</h1>
