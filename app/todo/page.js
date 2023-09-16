2b19798ccb370b17c5f1210651abbbc868803895
NTHOscar <72460275+NTHOscar@users.noreply.github.com>
2023-09-17 02:06:17 +0800

    try creating api

--- app/todo/page.js
+++ app/todo/page.js
@@ -7,8 +7,8 @@ async function getTODOList() {
     // const result = await db.records.getList('notes');
     try{
         const res = await fetch('http://127.0.0.1:8090/api/collections/todo/records?page=1&perPage=30', { cache: 'no-store' });
-        const data = await res.json();
-        console.log(data);
+        const data = await res.json(); 
+        console.log(JSON.parse(JSON.stringify(data.items)));
         return data?.items;
     } catch(err){
         console.log("Fetch failed!");
